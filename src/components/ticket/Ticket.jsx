7bcb3584ac243f2e9540507389e7d5d1ac545d3b
Pelin Karakoca <karakocapelin@gmail.com>
2023-01-01 22:19:54 +0300

    Style: add mobile compability

--- src/components/ticket/Ticket.jsx
+++ src/components/ticket/Ticket.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import TicketCard from './TicketCard';
-// flex-col-2 gap-40 px-40 pt-24 font-[Poppins]
-
+// <div className=" flex flex-col sm:justify-start pb-2"></div>
 function Ticket() {
   return (
-    <div
-      data-testid="ticket-1"
-      className="flex flex-row justify-center gap-4 border-pink-800 border-2"
-    >
-      <div className=" border-blue-500 border-2">
-        <h1 className="text-5xl">PURCHASE TICKETS</h1>
-        <p className="text-3xl">
+    <div className="flex flex-row px-2 sm:justify-center font-poppins">
+      <div className="border-2 sm:space-y-4">
+        <h1 className="text-2xl font-medium sm:font-normal sm:text-5xl ">
+          PURCHASE TICKETS
+        </h1>
+        <p className="text-xl font-medium mb-2 sm:mb-0 sm:font-normal sm:text-3xl">
           PURCHASE TICKETS THAT CAN BE USED TO BOOK APPOINMENTS!
         </p>
-        <div className="flex flex-row flex-wrap gap-8">
+        <div className="flex flex-row flex-wrap justify-center gap-6 sm:gap-8">
           <TicketCard ticketNumber="5" ticketPrice="10" />
           <TicketCard ticketNumber="25" ticketPrice="40" />
           <TicketCard ticketNumber="50" ticketPrice="70" />
--- src/components/ticket/TicketCard.jsx
+++ src/components/ticket/TicketCard.jsx
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom';
 
 function TicketCard({ ticketNumber, ticketPrice }) {
   return (
-    <div className="flex-col-3 w-80 h-80 pt-16 space-y-4 shadow-xl rounded-lg align-start text-center">
+    <div className="flex-col-4 sm:w-80 sm:h-80 w-40 h-40 pt-6 sm:pt-16 sm:space-y-4 space-y-2 sm:mt-4  shadow-2xl rounded-lg align-start text-center">
       <div>
-        <h1 className="text-5xl">{ticketNumber} TICKETS</h1>
+        <h1 className="text-2xl sm:text-5xl ">{ticketNumber} TICKETS</h1>
       </div>
       <div>
-        <span className="text-3xl text-[#0000008a]">{ticketPrice}$</span>
+        <span className="text-xl sm:text-3xl text-[#0000008a]">
+          {ticketPrice}$
+        </span>
       </div>{' '}
       <div>
         <Link to="/#">
           {' '}
           <button
-            className="w-52 h-16 self-center rounded-md bg-[#2DD3E3] hover:bg-[#3E64E9] text-2xl text-center leading-9 text-black "
+            className="sm:h-16 sm:w-52 w-26 h-8 self-center rounded-md bg-[#2DD3E3] hover:bg-[#3E64E9] hover:text-neutral-100 text-md px-2 sm:text-2xl text-center sm:leading-9 text-black "
             type="button"
           >
             PURCHASE
