1225121762bb78eb6646c4fb0da39c162f4ac954
Hadi <hadi.maleh@gmail.com>
2023-01-03 14:29:00 +0300

    Counseler card completed with responsive design and jest testing

--- src/components/Counsler/Counsler.jsx
+++ src/components/Counsler/Counsler.jsx
@@ -9,28 +9,23 @@ const Counsler = () => {
   }
 
   return (
-    <div className="absolute w-full h-full flex items-center justify-center py-4 md:py-8  ">
-      <div className="inline-flex flex-col space-y-3.5 items-center justify-start px-12 py-7 bg-white shadow border rounded-md border-gray-50">
-        <p
-          className="text-5xl text-center uppercase"
-          style={{ width: 1020, height: 51.43 }}
-        >
-          Are you a counselor?
-        </p>
-        <p
-          className="text-xl font-light text-center text-black text-opacity-90 lowercase"
-          style={{ width: 1020, height: 102.86 }}
-        >
+    <div
+      data-testid="counselor"
+      className="  flex justify-center font-poppins sm:w-[70%] md:w-[70%] lg:w-[70%] card self-center sm:ml-[1rem] md:ml-[10rem] lg:ml-[4rem]  "
+    >
+      <div className=" px-12 py-7   bg-white shadow border rounded-md  border-gray-50">
+        <p className=" text-5xl text-center uppercase">Are you a counselor?</p>
+        <p className="pt-[5%] pb-[6%] text-md font-poppins font-lights text-center  text-black text-opacity-90 lowercase">
           Interested in joining our mental health platform? You decide your
           schedule and how much you want to work, we’ll take care of the client
           referrals and billing details!
         </p>
-        <div className="w-48 h-1/4">
-          <div className="flex items-center justify-center flex-1 h-full px-6 py-3 bg-cyan-600 hover: bg-cyan- rounded-md">
+        <div className=" flex justify-center">
+          <div className=" object-center  font-poppins  h-full  lg:py-[0.7rem]  lg:px-[1rem]  md:py-[0.7rem]  md:px-[1rem] sm:p-[0.5rem] bg-cyan-400 hover:bg-cyan-600 rounded-md">
             <button
               type="button"
               onClick={handleClick}
-              className="flex-1 h-full text-2xl text-center uppercase"
+              className=" flex-initial  h-full text-md  text-center uppercase"
             >
               Learn More
             </button>
