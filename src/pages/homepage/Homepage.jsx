25d74f31017a67fcda5081a94e9b38d312254392
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-01-08 17:53:49 +0300

    issue4 complited

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Counsler from '../../components/Counsler/Counsler';
+import YellowSec from '../../components/hmpgYellowSection/YellowSection';
 import Ticket from '../../components/ticket/Ticket';
 
 const Homepage = () => {
@@ -7,6 +8,7 @@ const Homepage = () => {
     <div>
       <Ticket />
       <Counsler />
+      <YellowSec />
     </div>
   );
 };
