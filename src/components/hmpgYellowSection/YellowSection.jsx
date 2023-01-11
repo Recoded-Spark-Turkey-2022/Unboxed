dd0c9dfec15f47426a681474fa2de2824ebb8075
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-01-11 18:33:47 +0300

    Fixed mobile compatibility and hight problem

--- src/components/hmpgYellowSection/YellowSection.jsx
+++ src/components/hmpgYellowSection/YellowSection.jsx
@@ -3,22 +3,30 @@ import therapistpp from './therapistpp.svg';
 
 const YellowSec = () => {
   return (
-    <div data-testid="YellowSec" className="m-auto mb-1 pt-16 pr-72 flex flex-col px-2 tablet:justify-center font-poppins bg-[#FEE89E]">
-      <p className="text-5xl uppercase pb-12 pl-16">
-        Professional, licensed, and vetted therapists that you can trust
+    <div
+      data-testid="YellowSec"
+      className="mb-1 pt-16 flex flex-col px-1 font-poppins bg-[#FEE89E]"
+    >
+      <p className="text-5xl pt-[2rem] xl:pr-96 xl:mr-48 tablet:ml-36 tablet:pl-4 tablet:pr-2 tablet:mr-24 md:ml-12 md:pl-4 md:pr-36 sm:ml-4 sm:pl-2 sm:pr-12 uppercase">
+        Professional, licensed, and vetted therapists
       </p>
-      <img className="px-[3rem]" src={therapistpp} alt="therapistpp" />
-      <p className="p-[2rem] pl-16 text-xl font-light text-black">
-        {`Tap into the world's largest network of licensed, accredited, and
+      <p className="text-5xl pt-[2rem] xl:pr-96 xl:mr-48 tablet:ml-36 tablet:pl-4 tablet:pr-2 tablet:mr-24 md:ml-12 md:pl-4 md:pr-36 sm:ml-4 sm:pl-2 sm:pr-12 uppercase">that you can trust</p>
+      <img
+        className="pt-4 w-3/5  xl:ml-56 xl:pl-16 xl:pr-96 md:pl-12 tablet:ml-36 sm:ml-4 sm:pl-2 sm:pr-12 min-w-300px "
+        src={therapistpp}
+        alt="therapistpp"
+      />
+      <p className="pt-[2rem] xl:pr-96 xl:mr-48 tablet:ml-36 tablet:pl-4 tablet:pr-2 tablet:mr-24 md:ml-12 md:pl-4 md:pr-36 sm:ml-4 sm:pl-2 sm:pr-12 text-xl font-light text-black">
+          Tap into the world&apos;s largest network of licensed, accredited, and
           experienced therapists who can help you with a range of issues
           including depression, anxiety, relationships, trauma, grief, and more.
           with our therapists, you get the same professionalism and quality you
           would expect from an in-office therapist, but with the ability to
-          communicate whenever and however you want.`}
+          communicate whenever and however you want.
       </p>
       <button
         type="button"
-        className="p-[3rem] m-12 text-md md:text-lg ml-16 mr-96 max-w-lg lg:px-6 rounded-md transition duration-250 flex items-center justify-center h-full px-2 py-3 bg-[#2DD3E3] hover:bg-cyan-500"
+        className="p-[2rem] sm:ml-10 m-8 mb-16 text-md md:text-lg ml-44 mr-96 max-w-lg min-w-fit rounded-md transition duration-250 flex items-center justify-center h-full px-2 py-3 bg-[#2DD3E3] hover:bg-cyan-500"
       >
         <p className="flex-1 h-full text-2xl text-center uppercase text-ellipsis overflow-hidden">
           book an appointment
