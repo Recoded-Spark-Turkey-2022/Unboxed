e6c055a6e36fddd77d13c97c34dcef6b972f309b
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-01-11 06:50:49 +0300

    smaller screen text exposure fixed with a little cheat

--- src/components/hmpgCommunication/Comm.jsx
+++ src/components/hmpgCommunication/Comm.jsx
@@ -11,25 +11,25 @@ return(
     <h1 className="text-2xl font-medium tablet:font-normal tablet:text-5xl ">
         WE CAN COMMUNICATE THROUGH
     </h1>
-    <div className="flex flex-row flex-wrap justify-center gap-6 tablet:gap-8">
+    <div className="flex flex-row flex-wrap justify-center gap-6 tablet:gap-8 sm:gap-10">
         <div className="flex-col-4 tablet:w-80 tablet:h-80 w-40 h-40 pt-6 tablet:pt-16 tablet:space-y-4 space-y-2 tablet:mt-4  shadow-2xl rounded-lg align-start text-center" >
-            <img className="flex object-center mx-auto" src={phonecallimg} alt="phonecallimg" />
-            <h2 className="text-2xl tablet:text-5xl " >
+            <img className="flex object-center mx-auto h-1/3 w-1/3 md:h-1/3 md:w-1/3" src={phonecallimg} alt="phonecallimg" />
+            <h2 className="text-2xl sm:text-base font-semibold tablet:text-5xl " >
                 Voice Call 
             </h2>
-            <p>Feeling ready to start a conversation? Give your therapist a voice call and talk your heart out!</p>
+            <p className="pb-1 sm:text-xs">Feeling ready? Give your therapist a voice call and talk your heart out!</p>
         </div>
         <div className="flex-col-4 object-center tablet:w-80 tablet:h-80 w-40 h-40 pt-6 tablet:pt-16 tablet:space-y-4 space-y-2 tablet:mt-4  shadow-2xl rounded-lg align-start text-center">
-            <img className="flex object-center mx-auto" src={chatimg} alt="chatimg" />
-            <h2 className="text-2xl tablet:text-5xl"> 
+            <img className="flex object-center mx-auto h-1/3 w-1/3 md:h-1/3 md:w-1/3" src={chatimg} alt="chatimg" />
+            <h2 className="text-2xl sm:text-base font-semibold tablet:text-5xl"> 
                 Chat
             </h2>
-            <p>Need to talk to someone? Come have a chat with your therapist!</p>
+            <p className="pb-1 sm:text-xs">Need to talk to someone? Come have a chat with your therapist!</p>
         </div>
-        <div className="flex-col-4   tablet:w-80 tablet:h-80 w-40 h-40 pt-6 tablet:pt-16 tablet:space-y-4 space-y-2 tablet:mt-4  shadow-2xl rounded-lg align-start text-center">
-            <img className="flex object-center mx-auto" src={videocallimg} alt="videocallimg" />
-            <h2 className="text-2xl tablet:text-5xl" >Video Call</h2>
-            <p>For better experience therapists recommend video calls, but always remember that its a choice!</p>
+        <div className="flex flex-col items-center justify-evenly sm:w-40 sm:h-40 tablet:w-80 tablet:h-80 w-40 h-40 pt-6 tablet:pt-16 tablet:space-y-4 space-y-2 tablet:mt-4  shadow-2xl rounded-lg align-start text-center">
+            <img className="flex object-center mx-auto h-1/3 w-1/3 md:h-1/3 md:w-1/3," src={videocallimg} alt="videocallimg" />
+            <h2 className="text-2xl sm:text-base font-semibold tablet:text-5xl" >Video Call</h2>
+            <p className="pb-1 sm:text-xs">For better experience therapists recommend video calls, but this is a choice!</p>
         </div>
     </div>
   </div>  
