ad5c0e23184af390ef9f93871da2e6048a98934d
Nadim Absi <absinad1@gmail.com>
2023-01-12 15:40:11 +0300

    Fix: act(()=>)  issue now fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
+// import { doc } from "firebase/firestore";
 import { NavLink } from 'react-router-dom';
-// import { act } from 'react-dom/test-utils';
 import { auth } from '../../firebaseFile';
 
 const Navbar = () => {
   const [user, setUser] = useState({});
-
-  onAuthStateChanged(auth, (currentUser) => {
-    setUser(currentUser);
+   onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser){
+      // const userInfo = doc(db, 'patients', currentUser.uid);
+      setUser(currentUser);
+    }
   });
-
   const handleLogout = async () => {
     await signOut(auth);
+    setUser(null)
   };
   return (
     <div className="flex justify-around">
