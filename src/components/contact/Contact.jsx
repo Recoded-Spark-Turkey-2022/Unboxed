6476df19abedc06d9d358d65f0f6b568d9e995f5
Pelin Karakoca <karakocapelin@gmail.com>
2023-01-14 13:28:37 +0300

    rearrange component names

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import ContactThanks from './ContactThanks';
+// import ContactThanks from './ContactThanks';
+import ContactForm from './ContactForm';
 import ContactPlace from './ContactPlace';
+import contact from './contactImg.svg';
 
 function Contact() {
   return (
     <div>
-      <h1 className="flex-column flex-start text-5xl">SEND US YOUR REQUEST!</h1>
+      <h1 className="text-5xl">SEND US YOUR REQUEST!</h1>
       <text>
         Do you have a question, concern, idea, feedback, or problem? If you need
         assistance, please fill out the form below and we &apos d be happy to
         help!
       </text>
-      <ContactPlace />
+      <div className="flex-row border-2">
+        <div className="border-2 border-red-600 h-52 w-52">
+          {' '}
+          <ContactForm />
+        </div>
+        <div>
+          <aside>
+            <img
+              src={contact}
+              alt="smiling woman sitting in front of a computer with headphones, man on the background talking with the phone"
+            ></img>
+            <ContactPlace />
+          </aside>
+        </div>
+      </div>
     </div>
   );
 }
