57b1b37f419ce6b421f4e77e694bb74739930568
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-01-16 16:37:09 +0300

    .

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Counsler from '../../components/Counsler/Counsler';
+import HeroSec from '../../components/hmpgHeroSection/HeroSection';
 import Ticket from '../../components/ticket/Ticket';
 
 const Homepage = () => {
@@ -7,6 +8,7 @@ const Homepage = () => {
     <div>
       <Ticket />
       <Counsler />
+      <HeroSec />
     </div>
   );
 };
