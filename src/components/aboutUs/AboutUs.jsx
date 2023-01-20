257c8a7ca421a413e0c69de5f97c2fbefaf26d1e
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-01-20 20:22:43 +0300

    minor adjust

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -18,7 +18,7 @@ const HealingInfo = () => {
                 </p>
             </div>
             <div className="bg-[#EAF8F9] flex flex-row sm:flex-col items-center w-full ">
-                <img className="rounded-md tablet:my-8 laptop:ml-48 mr-12 tablet:ml-24 sm:ml-12 sm:w-3/4 sm:mt-8" src={FounderPP} alt="FounderPP"/>
+                <img className="rounded-md tablet:my-8 laptop:ml-48 mr-12 tablet:ml-16 sm:ml-12 tablet:w-1/2 sm:w-3/4 sm:mt-8" src={FounderPP} alt="FounderPP"/>
                 <div className="text-[#424A4F] flex flex-col sm:mx-12 tablet:my-16 sm:my-12">
                     <h1 className="laptop:text-5xl tablet:text-4xl sm:text-3xl mb-8 laptop:mr-52 tablet:mr-24">
                         Our Founding
