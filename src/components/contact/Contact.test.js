434094f6bc1894727d46be9d6f37f320f32a1410
Nadim Absi <absinad1@gmail.com>
2023-01-26 10:56:33 +0300

    Fix: tests

--- src/components/contact/Contact.test.js
+++ src/components/contact/Contact.test.js
@@ -4,12 +4,13 @@ import { act } from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
 
 test('Renders Contact Form component', () => {
+  act(() =>
     render(
       <BrowserRouter>
         <Contact />
       </BrowserRouter>
     )
-
+  );
   const contactFormElement = screen.getByTestId('contactForm');
   expect(contactFormElement).toBeInTheDocument();
 });
