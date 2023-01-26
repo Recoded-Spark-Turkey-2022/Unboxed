598fad3414b5ed8438bb84b4d2e9c7a2299335bb
esra akbulat <esra@klinikya.com>
2023-01-26 18:43:04 +0300

    issue43 was finished

--- src/routers/Routers.js
+++ src/routers/Routers.js
@@ -18,6 +18,7 @@ import Overview from '../pages/about/Overview';
 import Careers from '../pages/about/Careers';
 import BuyTickets from '../pages/buyTickets/BuyTickets';
 import PaymentMethods from '../pages/paymentMethods/PaymentMethods';
+import BuyTicketThankYou from '../pages/buyTicketThankYou/BuyTicketThankYou';
 
 const Routers = () => {
   return (
@@ -46,6 +47,7 @@ const Routers = () => {
         <Route path="about/careers" element={<Careers />} />
         <Route path="about/overview" element={<Overview />} />
         <Route path="buy-ticket" element={<BuyTickets />} />
+        <Route path="buy-ticket-thank-you" element={<BuyTicketThankYou />} />
         <Route path="payment-methods" element={<PaymentMethods />} />
       </Routes>
     </div>
