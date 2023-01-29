081eea93d5ee0e1064b53687e0cc65bff0fdfbe6
Nadim Absi <absinad1@gmail.com>
2023-01-29 18:46:50 +0300

    Addition of careers

--- src/pages/about/Careers.jsx
+++ src/pages/about/Careers.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { careerListingData } from '../../components/careerListings/careerListingData';
+import CareerListings  from '../../components/careerListings/CareerListings';
 
 const Careers = () => {
+  const handleClick = ()=>{
+    const element =document.getElementById('car')
+    if (element){
+      element.scrollIntoView({behavior:'smooth'})
+    }
+  }
   return (
     <div className=" font-poppins flex flex-col items-center ">
       <div className=" flex flex-col items-center my-10 w-9/12">
@@ -26,13 +34,14 @@ const Careers = () => {
           <button
             className="rounded text-xl px-7 py-3 relative top-8 sm:text-base font-medium shadow-md bg-cyan-400"
             type="button"
+            onClick={handleClick}
           >
             See our open list
           </button>
         </div>
       </div>
 
-      <div className=" bg-[#FEE89E] mt-24 mb-10 flex flex-col items-center">
+      <div className=" bg-[#FEE89E] mt-24 mb-5 flex flex-col items-center">
         <div className="flex flex-col w-9/12 py-3">
           <h1 className="text-[#424A4F] text-4xl mb-1">
             OUR HIRING PHILOSOPHY
@@ -45,6 +54,23 @@ const Careers = () => {
             representative slate of candidates before making an offer for our
             open roles.
           </p>
+        </div>
+      </div>
+
+      <div id="car" className=" flex flex-col items-center my-10 w-9/12">
+        <div className=" self-start">
+          <h1 className=" font-normal text-4xl mb-1">CURRENT OPEN POSITIONS</h1>
+          <p className="opacity-50 text-xl">
+            Please send us an email with the application title as the subject
+            with an attached CV in PDF format!
+          </p>
+          {careerListingData.map((career) => 
+            <CareerListings
+              title={career.title}
+              description={career.description}
+              position={career.position}
+            />
+          )}
         </div>
       </div>
     </div>
