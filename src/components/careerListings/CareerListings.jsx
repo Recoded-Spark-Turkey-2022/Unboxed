2d4d0d0bc84095779ebf6ad364799e26f6109642
esra akbulat <esra@klinikya.com>
2023-01-29 20:13:29 +0300

    last fixed

--- src/components/careerListings/CareerListings.jsx
+++ src/components/careerListings/CareerListings.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
+import { t } from 'i18next';
+import React from 'react';
 
-const CareerListings = ({title, description,position}) => {
+const CareerListings = ({ title, description, position }) => {
   return (
     <div className="flex justify-between  border-b-2  border-[#272733] ">
-    <div className="flex flex-col w-5/6">
-      <p className="text-[#2DD3E3] text-2xl sm:text-lg ">
-        {title}
-      </p>
-      <p className="text-[#424A4F] ">
-        {description}
-      </p>
+      <div className="flex flex-col w-5/6">
+        <p className="text-[#2DD3E3] text-2xl sm:text-lg ">{title}</p>
+        <p className="text-[#424A4F] ">{description}</p>
+      </div>
+      <div className="flex flex-col w-1/6 ">
+        <p className="text-[#2DD3E3] text-2xl sm:text-lg ">
+          {t('Engineering')}
+        </p>
+        <p className="">{position}</p>
+      </div>
     </div>
-    <div className="flex flex-col w-1/6 ">
-      <p className="text-[#2DD3E3] text-2xl sm:text-lg ">Engineering</p>
-      <p className="">{position}</p>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CareerListings
+export default CareerListings;
